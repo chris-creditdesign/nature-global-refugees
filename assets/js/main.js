1823f1d43c1ee1b054b7a262653a6028ad386388
Chris Ryan <creditdesign@gmail.com>
2017-02-09 13:01:50 +0000

    Adjusts colour of Latin America

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,7 +6,17 @@ CircularMigrationPlot({
 		mylayout: {
 			threshold: 2000,
 			labelThreshold: 5000,
-			colors: ['#FF0000','#ED8E00','#00B500','#683F91','#A1007E','#1F57A6','#11B2ED','#07E0CB','#638282','#FFDD00']
+			colors: [	'#FF0000',
+						'#ED8E00',
+						'#00B500',
+						'#683F91',
+						'#A1007E',
+						'#1F57A6',
+						'#11B2ED',
+						'#07E0CB',
+						'#638282',
+						'#FF7F00'
+					]
 			},
 		width: 630,
 		height: 630,
